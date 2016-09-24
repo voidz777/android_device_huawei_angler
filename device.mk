64872e55cba1d4ee5b00e06ac5ad3ed195a01fec
beanstown106 <nbenis106@gmail.com>
2016-09-25 02:01:50 +0300

    angler: update facelock properties
    Change-Id: Icb770c8ea626530d81bbea0edca6692545442916

--- device.mk
+++ device.mk
@@ -467,12 +467,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
   
 # facelock properties
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.facelock.black_timeout=400 \
-ro.facelock.det_timeout=1500 \
-ro.facelock.rec_timeout=2500 \
-ro.facelock.lively_timeout=2500 \
-ro.facelock.est_max_time=600 \
-ro.facelock.use_intro_anim=false
+    ro.facelock.black_timeout=700 \
+    ro.facelock.det_timeout=2500 \
+    ro.facelock.rec_timeout=3500 \
+    ro.facelock.est_max_time=600
 
 # OEM Unlock reporting
 ADDITIONAL_DEFAULT_PROPERTIES += \
