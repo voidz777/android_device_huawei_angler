8575054a6fce399c0bd28aba373e7ffa568e1807
Varun Date <date.varun123@gmail.com>
2016-09-25 14:08:00 +0530

    angler:Update fingerprint to NRD90U
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- cm.mk
+++ cm.mk
@@ -22,5 +22,5 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC20L/3230295:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC20L 3230295 release-keys"
+    BUILD_FINGERPRINT="google/angler/angler:7.0/NRD90U/3155372:user/release-keys" \
+PRIVATE_BUILD_DESC="angler-user 7.0 NRD90U 3155372 release-keys"
