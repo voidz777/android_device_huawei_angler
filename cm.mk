539ddd759378399b15d16a6abece4a0c92522fea
Varun Date <date.varun123@gmail.com>
2016-09-26 13:06:00 +0530

    Fix Sounds
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- cm.mk
+++ cm.mk
@@ -3,15 +3,10 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, device/huawei/angler/aosp_angler.mk)
 
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common.mk)
-
 # Inherit telephony stuff
-$(call inherit-product, vendor/cm/config/telephony.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
